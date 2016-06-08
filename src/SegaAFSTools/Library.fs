c03159521639f8bf9ec60f49df814ddbffe298ea
Erik Novales <enovales@codehitmen.com>
2016-06-08 06:54:36 -0700

    adding support for reading DGCP files

--- src/SegaAFSTools/Library.fs
+++ src/SegaAFSTools/Library.fs
@@ -5,12 +5,12 @@ open System.Diagnostics
 open System.IO
 open System.Text
 
-type ArchiveRawFileEntry = {
+type private RawFileEntry = {
     Offset: uint32
     Length: uint32
 }
 
-type ArchiveRawFilenameEntry = {
+type private RawFilenameEntry = {
     Name: string
     Year: uint16
     Month: uint16
@@ -42,7 +42,7 @@ let private readArchiveRawFilenameEntry(br: BinaryReader) =
         else
             Encoding.UTF8.GetString(nameBuf)
     {
-        ArchiveRawFilenameEntry.Name = nameString
+        RawFilenameEntry.Name = nameString
         Year = br.ReadUInt16()
         Month = br.ReadUInt16()
         Day = br.ReadUInt16()
@@ -53,7 +53,7 @@ let private readArchiveRawFilenameEntry(br: BinaryReader) =
     }
 
 
-type AFSArchive(s: Stream, br: BinaryReader, entries: ArchiveFileEntry array) = 
+type AFSArchive internal (s: Stream, br: BinaryReader, entries: ArchiveFileEntry array) = 
     new(s: Stream) = 
         let br = new BinaryReader(s, Encoding.UTF8, true)
 
@@ -65,7 +65,7 @@ type AFSArchive(s: Stream, br: BinaryReader, entries: ArchiveFileEntry array) =
         let numberOfFiles = br.ReadInt32()
 
         // read file entries.
-        let readNextFileEntry = (fun (_: int) -> { ArchiveRawFileEntry.Offset = br.ReadUInt32(); Length = br.ReadUInt32() })
+        let readNextFileEntry = (fun (_: int) -> { RawFileEntry.Offset = br.ReadUInt32(); Length = br.ReadUInt32() })
         let rawFileEntries = Seq.init(numberOfFiles) readNextFileEntry |> Array.ofSeq
 
         // filename table and length is the next entry.
@@ -87,7 +87,7 @@ type AFSArchive(s: Stream, br: BinaryReader, entries: ArchiveFileEntry array) =
         if (rawFilenameEntries.Length <> rawFileEntries.Length) then
             raise(exn("mismatch in number of file entries and filename entries"))
 
-        let fileEntries = rawFilenameEntries |> Array.zip(rawFileEntries) |> Array.map(fun (fe, fne) -> 
+        let makeArchiveFileEntry(fe: RawFileEntry, fne: RawFilenameEntry) = 
             {
                 ArchiveFileEntry.Name = fne.Name
                 Offset = fe.Offset
@@ -98,7 +98,13 @@ type AFSArchive(s: Stream, br: BinaryReader, entries: ArchiveFileEntry array) =
                 Hour = Some(fne.Hour)
                 Minute = Some(fne.Minute)
                 Second = Some(fne.Second)
-            })
+            }
+
+        let fileEntries = 
+            rawFilenameEntries 
+            |> Array.zip(rawFileEntries) 
+            |> Array.map makeArchiveFileEntry
+
         new AFSArchive(s, br, fileEntries)
 
     new(filename: string) = 
